55e552450bfc51298246881247414ed3e5fe98f9
Fred Weber <fweber@utk.edu>
2017-04-01 20:21:42 -0400

    Test

--- edit/edit.directive.js
+++ edit/edit.directive.js
@@ -1,19 +1,22 @@
-   angular
+(function () {
+    'use strict';
+    angular // jshint ignore:line
      .module('root')
 
-	.directive('file', function () {
+	 .directive('file', function () {
     	return {
-    		scope: {
-    		file: '='
-    	},
-        link: function (scope, el, attrs) {
-            unsubscribe.el.bind('change', function (event) {
-                var file = event.target.files[0];
-                scope.file = file ? file : undefined;
-            });
-	        el.on('$destroy', function() {
-				angular.element(el).off('change', unsubscribe);           
-			});
-        }
-        }
-        });
+            scope: {
+                file: '='
+            },
+            link: function (scope, el) {
+                unsubscribe.el.bind('change', function (event) { // jshint ignore:line
+                    var file = event.target.files[0];
+                    scope.file = file ? file : undefined;
+                });
+                el.on('$destroy', function () {
+                    angular.element(el).off('change', unsubscribe); // jshint ignore:line
+                });
+            }
+        };
+    });
+})();
